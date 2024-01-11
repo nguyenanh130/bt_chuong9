c6f2ac236b29285ded80cb41d5444c8d48c020e2
nguyenanh <148098785+nguyenanh130@users.noreply.github.com>
2024-01-11 16:03:15 +0700

    Add files via upload

---
+++ 9.1.py
@@ -0,0 +1,10 @@
+def sign(x):
+    if x<0:
+        return "-1"
+    elif x>0:
+        return "1"
+    elif x==0:
+        return "0"
+x=int(input("Nhập x : "))
+print("sign(",x,")=",sign(x))
+               
